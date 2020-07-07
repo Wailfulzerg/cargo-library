0c4a40271bca483ddc51840323283644b8acf372
Гаврилов Андрей <a.a.gavrilov@s7.ru>
2020-07-07 10:56:14 +0300

    working kafka avro consumer

--- cargo/src/kafka_manager/kafka_manager.py
+++ cargo/src/kafka_manager/kafka_manager.py
@@ -1,7 +1,12 @@
 import json
-import logging
-
+from loguru import logger
 from confluent_kafka import Producer, Consumer, KafkaError, KafkaException
+
+import io
+import struct
+from avro.io import BinaryDecoder, DatumReader
+from confluent_kafka.avro.cached_schema_registry_client import CachedSchemaRegistryClient
+from confluent_kafka.avro.serializer import SerializerError
 
 
 class KafkaProcessor(object):
@@ -28,9 +33,9 @@ class KafkaProcessor(object):
         """ Called once for each message produced to indicate delivery result.
             Triggered by poll() or flush(). """
         if err is not None:
-            logging.error('Message delivery failed: {}'.format(err))
+            logger.error('Message delivery failed: {}'.format(err))
         else:
-            logging.info('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+            logger.info('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
     def produce(self, messages):
         if not self.producer:
@@ -51,13 +56,84 @@ class KafkaProcessor(object):
                 if msg.error():
                     if msg.error().code() == KafkaError._PARTITION_EOF:
                         # End of partition event
-                        logging.info('%% %s [%d] reached end at offset %d\n' %
+                        logger.info('%% %s [%d] reached end at offset %d\n' %
                                      (msg.topic(), msg.partition(), msg.offset()))
                         break
                     elif msg.error():
                         raise KafkaException(msg.error())
                 else:
-                    logging.info(msg.value().decode('utf-8'))
+                    logger.info(msg.value().decode('utf-8'))
         finally:
             # Close down consumer to commit final offsets.
             self.consumer.close()
+
+
+class KafkaAvroProcessor(object):
+    def __init__(self, kafka_conf):
+        self.topic = kafka_conf['group.id']
+        self.register_client = None
+        self.consumer_conf = {
+            'auto.offset.reset': 'earliest',
+            'enable.partition.eof': True,
+        }
+        self.consumer_conf.update(kafka_conf)
+        # self.producer_conf = kafka_conf
+        # self.producer = None
+        self.consumer = None
+        self.MAGIC_BYTES = 0
+
+    def init_producer(self):
+        pass
+
+    def init_consumer(self, schema_registry_url, topics):
+        self.consumer = Consumer(self.consumer_conf)
+        self.register_client = CachedSchemaRegistryClient(url=schema_registry_url)
+        self.consumer.subscribe(topics)
+
+    @staticmethod
+    def delivery_report(err, msg):
+        """ Called once for each message produced to indicate delivery result.
+            Triggered by poll() or flush(). """
+        if err is not None:
+            logger.error('Message delivery failed: {}'.format(err))
+        else:
+            logger.\
+                info('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+    def produce(self, messages):
+        pass
+
+    def consume(self):
+        while True:
+            try:
+                msg = self.consumer.poll(1)
+            except SerializerError as e:
+                print("Message deserialization failed for {}: {}".format(msg, e))
+                raise SerializerError
+
+            if msg is None:
+                continue
+
+            if msg.error():
+                print("AvroConsumer error: {}".format(msg.error()))
+                return
+
+            key, value = self._unpack(msg.key()), self._unpack(msg.value())
+            print(key, value)
+
+    def _unpack(self, payload):
+        magic, schema_id = struct.unpack('>bi', payload[:5])
+
+        # Get Schema registry
+        # Avro value format
+        if magic == self.MAGIC_BYTES:
+            schema = self.register_client.get_by_id(schema_id)
+            reader = DatumReader(schema)
+            output = BinaryDecoder(io.BytesIO(payload[5:]))
+            abc = reader.read(output)
+            return abc
+        # String key
+        else:
+            # If KSQL payload, exclude timestamp which is inside the key.
+            # payload[:-8].decode()
+            return payload.decode()
---
+++ sandbox/fdw.py
@@ -0,0 +1,15 @@
+from cargo.src.kafka_manager.kafka_manager import KafkaAvroProcessor
+
+
+conf = {
+    "bootstrap.servers": "kafka1.m1test.eip.s7.aero:9093,kafka2.m1test.eip.s7.aero:9093,kafka3.m1test.eip.s7.aero:9093",
+    "ssl.ca.location": "/home/steel/work/cargo-library/new/CARoot.pem",
+    "ssl.certificate.location": "/home/steel/work/cargo-library/new/certificate.pem",
+    "ssl.key.location": "/home/steel/work/cargo-library/new/key.pem",
+    "group.id": 'cargoai2',
+    "security.protocol": "SSL"
+}
+
+c = KafkaAvroProcessor(conf)
+c.init_consumer(schema_registry_url="https://ksr-eip-rtc.s7.aero", topics=['FDW_FLIGHT_EVENT_UAT'])
+c.consume()
